25d7fad3e9e518df29f8d0371782cf9ef55fb3ac
Fiti-git <work@fathitech.com.lk>
2025-09-30 08:36:41 +0530

    stai rootadded

--- medirc_backend/settings.py
+++ medirc_backend/settings.py
@@ -142,3 +142,9 @@ CORS_ALLOW_ALL_ORIGINS = True  # ❗ Enable only in development. Restrict for pr
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
